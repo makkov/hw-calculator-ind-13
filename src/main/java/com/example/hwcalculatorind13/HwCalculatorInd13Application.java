36e360f2693c499a2c2dda4c147b000556cb6cb4
maks <maxim_86nva@mail.ru>
2023-08-19 11:59:06 +0300

    init project

---
+++ src/main/java/com/example/hwcalculatorind13/HwCalculatorInd13Application.java
@@ -0,0 +1,13 @@
+package com.example.hwcalculatorind13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCalculatorInd13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwCalculatorInd13Application.class, args);
+    }
+
+}
